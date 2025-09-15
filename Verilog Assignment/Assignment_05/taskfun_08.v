c17f8ec7da0dbeb2145527fe7701ec81a85fd414
Makesh Kumar <makeshkumar1910@gmail.com>
2025-09-15 11:53:04 +0530

    Add files via upload

---
+++ Verilog Assignment/Assignment_05/taskfun_08.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/10/2025 03:34:15 PM
+// Design Name: 
+// Module Name: taskfun_08
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module taskfun_08(
+
+    );
+endmodule
