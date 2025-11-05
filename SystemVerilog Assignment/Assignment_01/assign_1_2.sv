51454a70a7eb79c1caf285c21c4097957801736b
Makesh Kumar <makeshkumar1910@gmail.com>
2025-11-05 11:19:20 +0530

    Create assign_1_2.sv

---
+++ SystemVerilog Assignment/Assignment_01/assign_1_2.sv
@@ -0,0 +1,28 @@
+//Declare a global queue - "push_data" task should push data into the queue and "remove_data" task should pop data from the queue.
+
+module queue;
+int q[$];
+  
+task push_data;
+begin
+q.push_front(28);
+q.push_back(16);
+end
+endtask
+  
+task remove_data;
+begin
+q.pop_front();
+q.pop_back();
+end
+endtask
+  
+initial begin
+  q = '{12,28,5,16,18};
+  $display("Queue is: %0p",q);
+  push_data;
+  $display("Queue is: %0p",q);
+  remove_data;
+  $display("Queue is: %0p",q);
+end
+endmodule
